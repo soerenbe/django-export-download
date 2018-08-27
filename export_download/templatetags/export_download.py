6d698b4ee6222f6bc2da052e6d5f283b09bcbc2a
Sören Berger <soeren.berger@u1337.de>
2018-08-27 18:11:30 +0200

    Moved default button template in subdirectory

--- export_download/templatetags/export_download.py
+++ export_download/templatetags/export_download.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('export_download_menu.html', takes_context=True)
+@register.inclusion_tag('export_download/export_download_menu.html', takes_context=True)
 def resource_download_menu(context, button_class='btn-default'):
     return {
         'resources': context['view'].get_resource_links(context['request']),
